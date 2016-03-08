1dfb92023fc7d294496292ce5de2ae8c147f4d93
duynl17 <duynl17@gmail.com>
2016-03-08 16:35:37 -0500

    Update Question.java

--- Question.java
+++ Question.java
@@ -1,13 +1,14 @@
 /*Giovanni E. Desroches
  * Description: Classes representants une question, les questions seront entreposer dans
  * la classe database.
- * 
+ 	* @author: Duy, 2016/02/16
  */
 
 import java.util.ArrayList;
 
 /**
  * Class Question
+ * classe qui intialise les objets questions avec ses attributs, questions et réponses, avec les accesseurs et les mutateurs
  */
 public class Question {
 
@@ -16,6 +17,9 @@ public class Question {
     private String enonce;
 
     // Constructeurs
+    /**
+     *Constructeur qui initialise chaque objet question et permet d'identifier les énoncés et les réponses
+     */
     public Question(String unEnonce, ArrayList<String>desReponses){ 
     	
     	this.enonce = unEnonce;
@@ -24,23 +28,41 @@ public class Question {
     }
     
     //gets & sets
+    	/**
+	* Accesseur permettant de retourner la réponse
+	*/
 	public ArrayList<String> getReponses() {
 		return reponses;
 	}
-
+	/**
+	 * mutateur permettant de modifier une réponse
+	 * @param reponses
+	 * 	ArrayList de réponses
+	 */
 	public void setReponses(ArrayList<String> reponses) {
 		this.reponses = reponses;
 	}
-
+	/**
+	 *accesseur retournant l'énoncé de l'objet question
+	 */
 	public String getEnonce() {
 		return enonce;
 	}
-
+	/**
+	 *mutateur pour modifier l'énoncé de la question
+	 * @param enonce
+	 * 	String, énoncé, question posé
+	 */
 	public void setEnonce(String enonce) {
 		this.enonce = enonce;
 	}
 	
 	//Methodes
+	/**
+	 *accesseur pour retourner la réponse d'une question
+	 * @param pos
+	 * 	Int, chiffre qui identifie la position de la réponse dans le arrayList	
+	 */
 	public String getAnswerX(int pos){
 		return reponses.get(pos);
 	}
