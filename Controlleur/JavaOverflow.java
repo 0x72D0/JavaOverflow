80a21da7ad521f6fd5858ef00b0f3906a47aed4e
xeawrath <xeaprog@gmail.com>
2016-04-01 14:48:24 -0400

    bug fix

--- Controlleur/JavaOverflow.java
+++ Controlleur/JavaOverflow.java
@@ -38,62 +38,6 @@ public class JavaOverflow extends Application{
     //
     // Methods
     //
-<<<<<<< HEAD
-    
-=======
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
-    /**
-     * Initialise la base de donne en chargeant les fichiers questions. 
-     * @return void
-     */
-	public static void generateQuestion()
-<<<<<<< HEAD
-	{
-		Random rand = new Random();
-		int i = rand.nextInt(database.getQuestions().size()-1);
-		cwq = database.getQuestions().get(i);
-	}
- 
-    /**
-     * formatte le String de la reponse et de la question pour tester leur egalite
-     * @param str1
-     * @return le string une fois formatter 
-     */
-	public static String formatString(String str1)
-	{
-        str1 = str1.toLowerCase();
-        StringBuffer str = new StringBuffer(str1);
-        for(int i = 0; i < str.length(); i++)
-        {
-            char a = str.charAt(i);
-            
-            switch(a)
-            {
-                case 224:str.setCharAt(i, 'a');
-                break;
-                case 226: str.setCharAt(i, 'a');
-                break;
-                
-                case 232: str.setCharAt(i, 'e');
-                break;
-                case 233: str.setCharAt(i, 'e');
-                break;
-                case 234: str.setCharAt(i, 'e');
-                break;
-                case 235: str.setCharAt(i, 'e');
-                break;
-                
-                case 238: str.setCharAt(i, 'i');
-                break;
-                case 239: str.setCharAt(i, 'i');
-                break;
-                
-                case 251: str.setCharAt(i, 'u');
-                break;
-            }
-        }
-		return str.toString();
-	}
     
     /**
      * Verifie une reponse entre en parametre en la comparant avec les reponses
@@ -101,8 +45,9 @@ public class JavaOverflow extends Application{
      * @param rep Une reponse  de l'utilisateur
      * @return Vrai si la reponse est vrai, faux dans le cas contraire.
      */
-=======
-	{	boolean allDone = true;
+	public static void generateQuestion()
+	{
+        boolean allDone = true;
 		for(Question q:database.getQuestions())
 		{	
             if(!q.isDone())
@@ -173,8 +118,6 @@ public class JavaOverflow extends Application{
                     break;
             }
         }
-        
-        
 		return buf.toString();
 	}
 	
@@ -184,7 +127,6 @@ public class JavaOverflow extends Application{
     * @param rep Une reponse  de l'utilisateur
     * @return Vrai si la reponse est vrai, faux dans le cas contraire.
     */
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
 	public static boolean verifyStringAnswer(String rep)
 	{	
 		// dans cet algorithme rep est la reponse entree par l'utilisateur
@@ -208,17 +150,10 @@ public class JavaOverflow extends Application{
 	}
 	
     /**
-<<<<<<< HEAD
-     * prends la reponse modele et la reponse donne par l'utiliseur et regarde leurs egalites
-     * @param rep
-     * @param ans
-     * @return true si la reponse modele est egal a la reponse donne par l'utilisateur sinon false
-=======
      * verifie la reponse modele avec la reponse de l'utilisateur
      * @param rep
      * @param ans
      * @return true si l'egalite est vrai, sinon false 
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
      */
 	public static boolean verifyFormatAnswer(String rep, String ans)
 	{
@@ -259,24 +194,13 @@ public class JavaOverflow extends Application{
 	}
 	
     /**
-<<<<<<< HEAD
      * calcule le niveau de la categorie pour aider a choisir les futurs questions
      * @param category
      * @return retourne le niveau de la categorie
-=======
-     * calcule le niveau de la categorie
-     * @param category
-     * @return le niveau de la categorie
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
      */
 	public static short categoryLevel(String category)
     {
-        
-<<<<<<< HEAD
         for(short level = 1;level < 101;level++)
-=======
-        for(short level =1;level<101;level++)
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
         {
             for(Question q:database.getQuestions())
             {
@@ -284,18 +208,10 @@ public class JavaOverflow extends Application{
                 {
                     if(q.getCategory().equals(category))
                     {
-<<<<<<< HEAD
-=======
-
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
                         if(!q.isDone())
                         {
                             return level;
                         }
-<<<<<<< HEAD
-=======
-
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
                     }
                 }
             }
@@ -311,15 +227,11 @@ public class JavaOverflow extends Application{
     //
     // Other methods
     //
-<<<<<<< HEAD
     
     /**
      * le main du programme
      * @param args 
      */
-=======
-
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
     public static void main(String[]args)
     {
     	
@@ -334,15 +246,8 @@ public class JavaOverflow extends Application{
 			
 			//new MainMenu(); Plus besoin vu que on utilize pas swing
 			
-<<<<<<< HEAD
-			launch(args); //Lance Java
-=======
 			launch(args); //Lance JavaFx
-			
-			
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
 		}
-		
 		catch(Exception e)
 		{
 			e.printStackTrace();
@@ -374,20 +279,9 @@ public class JavaOverflow extends Application{
 		alert.setContentText("Etes-vous sur de vouloir fermer?");
 		
 		Optional<ButtonType> result = alert.showAndWait();
-<<<<<<< HEAD
 		if (result.get() == ButtonType.OK)
         {
 			theStage.close();
 		}
 	}
-=======
-		if (result.get() == ButtonType.OK){
-			theStage.close();
-		} else {
-		   
-		}
-	}
-	
-
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
 }
--- Modele/Question.java
+++ Modele/Question.java
@@ -20,13 +20,7 @@ public class Question implements Serializable{
     short difficulty;
     boolean done;
     // Constructeurs
-<<<<<<< HEAD
     
-=======
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
-    /**
-     *Constructeur qui initialise chaque objet question et permet d'identifier les énoncés et les réponses
-     */
     public Question(String unEnonce, ArrayList<String>desReponses,String category,short difficulty,String bonFeedBack,String mauvaisFeedBack,boolean done){
     	
     	this.enonce = unEnonce;
@@ -39,48 +33,30 @@ public class Question implements Serializable{
     }
     
     //gets & sets
-<<<<<<< HEAD
     
     /**
 	* Accesseur permettant de retourner la réponse
     * @return l'array de reponse
-=======
-    	/**
-	* Accesseur permettant de retourner la réponse
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
 	*/
 	public ArrayList<String> getReponses() {
 		return reponses;
 	}
-<<<<<<< HEAD
-    
 	/**
 	 * mutateur permettant de modifier une réponse
 	 * @param reponses
-=======
-	/**
-	 * mutateur permettant de modifier une réponse
-	 * @param reponses
-	 * 	ArrayList de réponses
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
 	 */
 	public void setReponses(ArrayList<String> reponses) {
 		this.reponses = reponses;
 	}
-<<<<<<< HEAD
     
 	/**
      * accesseur retournant l'énoncé de l'objet question
      * @return l'enoncer de la question
      */
-=======
-	/**
-	 *accesseur retournant l'énoncé de l'objet question
-	 */
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
 	public String getEnonce() {
 		return enonce;
 	}
+    
 	/**
 	 *mutateur pour modifier l'énoncé de la question
 	 * @param enonce
@@ -93,11 +69,7 @@ public class Question implements Serializable{
 	//Methodes
 	/**
 	 *accesseur pour retourner la réponse d'une question
-	 * @param pos
-<<<<<<< HEAD
-=======
-	 * 	Int, chiffre qui identifie la position de la réponse dans le arrayList	
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
+	 * @para<<<<<<< HEADm pos
 	 */
 	public String getAnswerX(int pos){
 		return reponses.get(pos);
@@ -113,11 +85,7 @@ public class Question implements Serializable{
 
     /**
      * modifie le bon feedback
-<<<<<<< HEAD
      * @param bonFeedBack
-=======
-     * @param bonFeedBack nouveau feedback
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
      */
     public void setBonFeedBack(String bonFeedBack) {
         this.bonFeedBack = bonFeedBack;
@@ -135,12 +103,8 @@ public class Question implements Serializable{
      * change le mauvais feedback
      * @param mauvaisFeedBack nouveau feedback
      */
-<<<<<<< HEAD
-    public void setMauvaisFeedBack(String mauvaisFeedBack) {
-=======
     public void setMauvaisFeedBack(String mauvaisFeedBack)
     {
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
         this.mauvaisFeedBack = mauvaisFeedBack;
     }
 
@@ -148,12 +112,8 @@ public class Question implements Serializable{
      * retourne la categorie actuelle
      * @return category actuelle
      */
-<<<<<<< HEAD
-    public String getCategory() {
-=======
     public String getCategory()
     {
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
         return category;
     }
 
@@ -161,12 +121,8 @@ public class Question implements Serializable{
      * modifie la category
      * @param category nouvelle categorie
      */
-<<<<<<< HEAD
-    public void setCategory(String category) {
-=======
     public void setCategory(String category)
     {
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
         this.category = category;
     }
 
@@ -174,12 +130,8 @@ public class Question implements Serializable{
      * retourne la difficulte actuelle
      * @return difficulty la difficulte actuelle
      */
-<<<<<<< HEAD
-    public short getDifficulty() {
-=======
     public short getDifficulty()
     {
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
         return difficulty;
     }
 
@@ -187,37 +139,23 @@ public class Question implements Serializable{
      * modifier la difiiculte actuelle
      * @param difficulty
      */
-<<<<<<< HEAD
-    public void setDifficulty(short difficulty) {
-=======
     public void setDifficulty(short difficulty)
     {
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
         this.difficulty = difficulty;
     }
     
     /**
-<<<<<<< HEAD
-     * mutateur de done
-=======
      * mutateur de la variable done
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
      * @param done 
      */
     public void setDone(boolean done)
     {
         this.done=done;
     }
-<<<<<<< HEAD
     
-    /**
-     * acesseur de done
-     * @return retourne la variable done
-=======
     /**
      * acesseur de la variable done
      * @return la valeur de done 
->>>>>>> 973bacf3f1323b2baa302b2065e5469c1a08a4ec
      */
     public boolean isDone()
     {
